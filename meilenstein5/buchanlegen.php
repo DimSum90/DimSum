c934a2997039e7dd64a6390945569328376fe8ce
patrick-binaryHospital <patrick_buessecker@web.de>
2015-06-21 10:13:53 +0200

    Changed: book_entry.php, Werte der Checkboxen zur Buchart angepasst, damit diese mit einem Großbuchstaben beginnen, Meilenstein 5 Aufgabe 1, p.buessecker Changed: buchanlegen.php, Eingaben des Benutzers werden jetzt erst in Variablen geschrieben, damit diese schon angepasst werden koennen, Meilenstein 5 Aufgabe 1, p.buessecker

--- meilenstein5/buchanlegen.php
+++ meilenstein5/buchanlegen.php
@@ -1,6 +1,15 @@
 <?php
 
-$datensatz = $_GET[autor] . ", " . $_GET[titel].", ".$_GET[kapitel].", ".$_GET[art].", ". $_GET[isbn].", ".$_GET[jahr].", ". $_GET[auflage].";\n" ;
+$autor = $_GET["autor"];
+$titel = $_GET["titel"];
+$kapitel = $_GET["kapitel"]." Kapitel";
+$art = $_GET["art"];
+$isbn = $_GET["isbn"];
+$jahr = $_GET["jahr"];
+$auflage = $_GET["auflage"].".Auflage";
+
+$datensatz = $autor.", ".$titel.", ".$kapitel.", ".$art.", ".$isbn.", ".$jahr.", ".$auflage.";\n";
+
 $datei = fopen("books.txt","a");
 fwrite($datei, $datensatz);
 fclose($datei);
